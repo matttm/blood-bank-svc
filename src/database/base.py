57a792d0830b0b5cb8281f33597a179a48e295fb
matttm <matttmaloney@gmail.com>
2023-03-12 13:56:54 -0400

    establishing db conn

--- src/database/base.py
+++ src/database/base.py
@@ -3,8 +3,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+import sys
 
-engine = create_engine('postgresql://usr:pass@localhost:5432/sqlalchemy')
+env_vars = [
+    'DB_HOST': None,
+    'DB_PORT': None,
+    'DB_NAME'; None,
+    'DB_USER': None,
+    'DB_PASSWORD': None
+]
+for var in env_vars:
+    tmp = os.environ.get(var)
+    if tmp is None:
+        print(f'Error: {tmp} is not defined')
+        sys.exit()
+    env_vars[var] = tmp
+
+engine = create_engine(f'mysql://{env_vars['DB_USER']}:{env_vars['DB_PASSWORD']}@{env_vars['DB_HOST']}:{env_vars['DB_PORT']}/{env_vars['DB_NAME']}')
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
