d7a62e43fdd650501daffd4c3a2e8e6805b13c8b
matttm <matttmaloney@gmail.com>
2023-03-14 18:39:36 -0400

    adding init

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,8 +1,8 @@
 from __future__ import print_function
-import services.event_handler as event_handler
+from services import event_handler
 
 def lambda_handler(event, context):
     for record in event['Records']:
-        event_handler(record)
+        event_handler.event_handler(record)
         payload = record["body"]
         print(str(payload))
