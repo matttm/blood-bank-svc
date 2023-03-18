a45553d207fccef9dfd0fd894c7d9cfc7f4fe203
matttm <matttmaloney@gmail.com>
2023-03-18 12:40:48 -0400

    removing match

--- src/event_handler.py
+++ src/event_handler.py
@@ -3,13 +3,12 @@ from database.models import donor
 
 def  event_handler(event):
     event_cd = event.cd
-    match (event_cd):
-        case 'NDA':
-            _donor = event.donor
-            model = donor.Donor(_donor.fname, _donor.lname, _donor.bloodType)
-            session.add(model)
-            session.commit()
-            print('new donor')
-        case _:
-            print('Unknown code')
+    if event_cd == 'NDA':
+        _donor = event.donor
+        model = donor.Donor(_donor.fname, _donor.lname, _donor.bloodType)
+        session.add(model)
+        session.commit()
+        print('new donor')
+    else:
+        print('Unknown code')
     session.close()
