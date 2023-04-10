ad0b2c744205d691df1fc264064ca8fa1e7398ba
matttm <matttmaloney@gmail.com>
2023-04-10 07:15:37 -0400

    adding date fields and amount and email

--- src/database/models/transaction.py
+++ src/database/models/transaction.py
@@ -9,9 +9,13 @@ class Transaction(Base):
 
     transaction_id = Column('TRANSACTION_ID', Integer, primary_key=True)
     transaction_type = Column('TRANSACTION_TYPE', String)
+    blood_amount_ml = Column('BLOOD_AMOUNT_ML', String)
     donor_id = Column('DONOR_ID', Integer)
+    created_at = Column('CREATED_AT', Date)
+    updated_at = Column('UPDATED_AT', Date)
 
-    def __init__(self, transaction_id, transaction_type, donor_id):
+    def __init__(self, transaction_id, transaction_type, blood_amount_ml, donor_id):
         self.transaction_id = transaction_id
         self.transaction_type = transaction_type
+        self.blood_amount_ml = blood_amount_ml
         self.donor_id = donor_id
